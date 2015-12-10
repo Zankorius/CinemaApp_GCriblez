47314f1f7ae321dcf25e6efd995bf43031edc7e3
Gary Criblez <g.criblez@gmail.com>
2015-12-10 08:52:42 +0100

    Ajout des classes business Movie et Person et la classe de Service

---
+++ CinemaApp_GCriblez/src/java/ch/hegarc/ig/odi/cinemaapp/business/Movie.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hegarc.ig.odi.cinemaapp.business;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Classe métier Movie
+ * @author gary.criblez
+ */
+public class Movie implements Serializable {
+    private Long id;
+    private String name;
+    private String producer;
+    Map<Long,Person> persons;
+    
+    /**
+     * Constructeur Movie sans paramètre
+     */
+    public Movie() {
+    }
+    
+    /**
+     * Constructeur Movie
+     * Param id, name, producer
+     * Initialise la map pour les personnes ayant vue le film
+     */
+    public Movie(Long id, String name, String producer) {
+        this.id = id;
+        this.name = name;
+        this.producer = producer;
+        this.persons = new LinkedHashMap();
+    }
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public Map<Long, Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(Map<Long, Person> persons) {
+        this.persons = persons;
+    }
+    
+    
+    
+}
---
+++ CinemaApp_GCriblez/src/java/ch/hegarc/ig/odi/cinemaapp/business/Person.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hegarc.ig.odi.cinemaapp.business;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ *Classe métier Person
+ * @author gary.criblez
+ */
+public class Person implements Serializable {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    Map<Long,Movie> movies;
+    
+    /**
+     * Constructeur Person sans paramètre
+     */
+    public Person() {
+    }
+    
+    /**
+     * Constructeur Person
+     * Param id, firstName, lastName
+     * Initialise la map pour les films vus
+     */
+    public Person(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.movies = new LinkedHashMap();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Map<Long, Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Map<Long, Movie> movies) {
+        this.movies = movies;
+    }
+    
+    /**
+     * Ajoute le film à la personne et la personne au film
+     */
+    public void addMovie(Movie movie) {
+        movie.getPersons().put(id, this);
+        movies.put(movie.getId(), movie);
+    }
+    
+    
+    
+    
+}
